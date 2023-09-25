93001f2cc5bf9b2eecf018b30cef8574f827b436
JairoJJr <jairojannini.contato@gmail.com>
2023-09-24 22:43:42 -0300

    ainda muita coisa errada

--- Relógio2.0/controllers/__init__.py
+++ Relógio2.0/controllers/__init__.py
@@ -4,5 +4,4 @@ from views import itens , Tela
 class  C_controller:
     def __init__(self,root):
         self.tela = Tela(root)
-        self.data = itens(root).get_data()
-        self.hora = itens(root).get_hora()
+        self.itens = itens(root)
--- Relógio2.0/views/__init__.py
+++ Relógio2.0/views/__init__.py
@@ -10,24 +10,38 @@ class Tela:
 
 class itens:
     def __init__(self,root):
+        #labels
         self.data = Label(root , bg = estilo.cor1 , fg = estilo.cor2 , font=estilo.fonte1)
-        self.data.pack(pady=10)
+        #self.data.pack(pady=10)
 
         self.hora = Label(root , bg = estilo.cor1 , fg = estilo.cor2 , font = estilo.fonte2 )
         self.hora.pack()
+        
         self.timer = Label(root , bg = estilo.cor1 , fg = estilo.cor2 , text = "00:00" , font=estilo.fonte3)
         self.timer.pack()
-    
-    def get_data(self):
-        data_atual = strftime('%a, %d, %b, %Y')
-        self.data.config(text=data_atual)
 
-    def get_hora(self):
-        hora_atual = strftime("%H:%M:%S")
-        self.hora.config(text=hora_atual)
-        self.hora.after(10,self.get_hora)
-        
-    
-    
-    
+        self.text_min = Label(root , bg = estilo.cor1 , fg = estilo.cor2 , text = "min" , font = estilo.fonte1)
+        #self.text_min.place(x = 350 , y = 227)
 
+        #botão
+        #Função do botão
+        def iniciar():
+            print("ok")
+        self.botao_start = Button(root, text = "Iniciar", bg = estilo.cor1, fg = estilo.cor2, command = iniciar)
+        self.botao_start.pack()
+    
+        #Função para buscar data e hora local
+        def get_data():
+            data_atual = strftime('%a, %d, %b, %Y')
+            self.data.config(text=data_atual)
+        get_data()
+
+        def get_hora():
+            hora_atual = strftime("%H:%M:%S")
+            self.hora.config(text=hora_atual)
+            self.hora.after(10,get_hora)
+        get_hora()
+
+
+"""class count_down:
+    def __init__(self,root)"""
